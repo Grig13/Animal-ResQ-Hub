55123ee58b304a080a56d71c4caf84fbb3d50019
MihaiChilom <mihai.chilom@omeron.co>
2023-12-06 20:05:50 +0200

    mihai-branch
    main page + ather style added

--- Application_UI/application-ui/src/app/app.module.ts
+++ Application_UI/application-ui/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule, DatePipe } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgToastModule } from 'ng-angular-popup';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { DatePipe } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgToastModule } from 'ng-angular-popup'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
-import { TokenInterceptor } from './interceptors/token.interceptor';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,8 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
     ResetPasswordComponent,
   ],
   imports: [
+    MatButtonModule,
+    MatMenuModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -38,18 +41,18 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       timeOut: 1000,
-      progressBar : true,
+      progressBar: true,
       progressAnimation: 'increasing',
       preventDuplicates: true
     }),
     FormsModule
   ],
   providers: [LoginComponent,
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptor,
-    multi: true
-  }],
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Application_UI/application-ui/src/app/components/navbar/navbar.component.ts
+++ Application_UI/application-ui/src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false; // Set to true if the user is logged in, otherwise false
+  showDropdown: boolean = false;
 
+  login() {
+    // Implement your login logic here
+    this.isLoggedIn = true;
+  }
+
+  logout() {
+    // Implement your logout logic here
+    this.isLoggedIn = false;
+  }
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
 }
